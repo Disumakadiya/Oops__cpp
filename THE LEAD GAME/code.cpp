ca65df20f3b362c22b8ce2aa8fef4ee9c379dd3b
Disumakadiya <disumakadiya@gmail.com>
2025-05-02 10:48:15 +0530

    THE LEAD GAME

--- THE LEAD GAME/code.cpp
+++ THE LEAD GAME/code.cpp
@@ -2,35 +2,39 @@
 #include <cmath>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
-    cout<<"ENTER THE NUMBER  OF ROUNDS IN LEAD GAME:";
+    cout << "ENTER THE NUMBER  OF ROUNDS IN LEAD GAME:";
     cin >> n;
-
-    if (n <= 0 || n > 1000) {
-        cout << "Invalid number of rounds. N must be between 1 and 1000." << endl;
-    } else {
+    {
         int maxLead = 0, winner = 0;
         int player1Total = 0, player2Total = 0;
 
-        for (int i = 0; i < n; ++i) {
-            int p1, p2;
-            cin >> p1 >> p2;
-            cout<<"the score of player 1::"<<p1<<endl;
-            cout<<"the score of player 2::"<<p2<<endl;
+        for (int i = 1; i <= n; ++i)
+        {
+            int s1, s2;
+            cout << "ENTER THE SCORE OF " << i << " ROUND:" << endl;
+            cin >> s1 >> s2;
+            cout << "the score of player 1::" << s1 << endl;
+            cout << "the score of player 2::" << s2 << endl;
 
-            player1Total += p1;
-            player2Total += p2;
+            player1Total += s1;
+            player2Total += s2;
 
-            int currentLead = abs(player1Total - player2Total);
-            if (currentLead > maxLead) {
-                maxLead = currentLead;
-                winner = (player1Total > player2Total) ? 1 : 2;
+            int lead = player1Total - player2Total;
+            int absLead = abs(lead);
+
+            if (absLead > maxLead)
+            {
+                maxLead = absLead;
+                winner = (lead > 0) ? 1 : 2;
             }
         }
-
-        cout <<"The winner among the two player is:player-"<< winner << endl<<"The lead score by which the player is won--" << maxLead << endl;
+        cout << "*****THE FINAL RESULT******" << endl;
+        cout << "The winner among the two player is:player-" << winner << endl
+             << "The lead score by which the player is won--" << maxLead << endl;
     }
-    
+
     return 0;
 }
